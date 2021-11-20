54f2120de1151cb4f54fca1861009227e37755b0
Benzolamps <gwy9826@163.com>
2021-11-20 23:44:35 +0800

    提交

---
+++ Content/Talos/Levels/sc_level_pack/Scripts/TimeSwitchEventProvider.lua
@@ -0,0 +1,31 @@
+local util = worldGlobals.CreateUtil(worldInfo)
+
+local lastState = 0
+
+RunHandled(
+  WaitForever,
+  OnEvery(Delay(0.1)),
+  function()
+    if util.IsTimeSwitchRecording() then
+      if (lastState == 0) then
+        SignalEvent("TimeSwitchRecordingStarted")
+        lastState = 1
+      end
+    elseif util.IsTimeSwitchPlaying() then
+      if (lastState == 1) then
+        SignalEvent("TimeSwitchRecordingEnded")
+        SignalEvent("TimeSwitchPlayingStarted")
+        lastState = 2
+      end
+    else
+      if (lastState == 1) then
+        SignalEvent("TimeSwitchRecordingEnded")
+        SignalEvent("TimeSwitchRecordingAborted")
+        lastState = 0
+      elseif (lastState == 2) then
+        SignalEvent("TimeSwitchPlayingEnded")
+        lastState = 0
+      end
+    end
+  end
+)
