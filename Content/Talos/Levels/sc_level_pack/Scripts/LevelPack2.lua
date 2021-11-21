7ee40cf198c2e45a2f19683b16f2c9f218cee7a9
Benzolamps <gwy9826@163.com>
2021-11-21 12:47:10 +0800

    提交

--- Content/Talos/Levels/sc_level_pack/Scripts/LevelPack2.lua
+++ Content/Talos/Levels/sc_level_pack/Scripts/LevelPack2.lua
@@ -1,2 +1,2 @@
-worldGlobals.CreateLevel(TranslateString("TTRS:ScLevelPack.Level19=Code Across Time and Space"), "T", "Content/Talos/Levels/sc_level_pack/level19.wld", 19)
+worldGlobals.CreateLevel(TranslateString("TTRS:ScLevelPack.Level19=Code across Time and Space"), "T", "Content/Talos/Levels/sc_level_pack/level19.wld", 19)
 worldGlobals.CreateLevel(TranslateString("TTRS:ScLevelPack.Level20=Split Personality"), "TFC", "Content/Talos/Levels/sc_level_pack/level20.wld", 20)
