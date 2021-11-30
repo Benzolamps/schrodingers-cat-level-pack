340b28be581c661a16b3cd51fb825800a73a9363
Benzolamps <gwy9826@163.com>
2021-11-30 21:32:40 +0800

    提交

--- Content/Talos/Levels/sc_level_pack/Scripts/Level017Reset2.lua
+++ Content/Talos/Levels/sc_level_pack/Scripts/Level017Reset2.lua
@@ -4,11 +4,11 @@ RunHandled(
   util.WaitTerminal,
   OnEvery(Delay(0.1)),
   function ()
+    if red:IsChargedUp() then return end
     if util.IsTimeSwitchActive() then return end
     local itemCount = util.EntityCountInArea("CCarriableItemEntity", entityDetector)
     local jammerCount = util.EntityCountInArea("CJammerItemEntity", entityDetector)
     if itemCount == jammerCount then return end
-    if red:IsChargedUp() then return end
     if playerDetector1:IsActivated() or playerDetector2:IsActivated() then
       playerDetector1:Recharge()
       playerDetector2:Recharge()
