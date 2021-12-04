252395efb49e113ff5ebe410a2d2b2995554ca3a
Benzolamps <gwy9826@163.com>
2021-12-04 23:28:51 +0800

    提交

--- Content/Talos/Levels/sc_level_pack/Scripts/Level016Reset.lua
+++ Content/Talos/Levels/sc_level_pack/Scripts/Level016Reset.lua
@@ -5,8 +5,12 @@ RunHandled(
   OnEvery(Event(detector.Activated)),
   function()
     detector:Recharge()
-    if plasma:IsOpen() then return end
-    if fan:IsActive() then return end
+    if util.IsTimeSwitchPlaying() then return end
+    if receivers[1]:IsChargedUp()
+      or receivers[1]:IsCharging()
+      or receivers[2]:IsChargedUp()
+      or receivers[2]:IsCharging()
+    then return end
     if util.ExistEntityInArea("CCarriableRodItemEntity", detector) then return end
     util.ResetMessage()
   end
