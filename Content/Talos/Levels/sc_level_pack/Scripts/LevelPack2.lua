8d46fd198b6eabd7ac6594a2d5c0a19bea54dd1f
Benzolamps <gwy9826@163.com>
2021-12-05 13:18:55 +0800

    提交

--- Content/Talos/Levels/sc_level_pack/Scripts/LevelPack2.lua
+++ Content/Talos/Levels/sc_level_pack/Scripts/LevelPack2.lua
@@ -2,3 +2,4 @@ worldGlobals.CreateLevel(TranslateString("TTRS:ScLevelPack.Level19=Code across T
 worldGlobals.CreateLevel(TranslateString("TTRS:ScLevelPack.Level20=Split Personality"), "TFC", "Content/Talos/Levels/sc_level_pack/level20.wld", 20)
 worldGlobals.CreateLevel(TranslateString("TTRS:ScLevelPack.Level21=Plagued by Crises"), "FCR", "Content/Talos/Levels/sc_level_pack/level21.wld", 21)
 worldGlobals.CreateLevel(TranslateString("TTRS:ScLevelPack.Level22=Domino"), "C", "Content/Talos/Levels/sc_level_pack/level22.wld", 22)
+worldGlobals.CreateLevel(TranslateString("TTRS:ScLevelPack.Level23=Hermit"), "R", "Content/Talos/Levels/sc_level_pack/level23.wld", 23)
