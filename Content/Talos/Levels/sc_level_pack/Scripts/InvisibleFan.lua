96fd399a0b48ceacaab78f41647540bc06cd8146
Benzolamps <gwy9826@163.com>
2021-12-24 13:40:50 +0800

    变更

--- Content/Talos/Levels/sc_level_pack/Scripts/InvisibleFan.lua
+++ Content/Talos/Levels/sc_level_pack/Scripts/InvisibleFan.lua
@@ -1,31 +1,13 @@
 local util = worldGlobals.CreateUtil()
 
-local switch, fan
-
-for _, entity in ipairs(switchesAndFans) do
-  if entity:GetClassName() == "CFanEntity" then
-    fan = entity
-    local initActivated = not not switch:IsActivated()
-    RunAsync(function()
-      RunHandled(
-        WaitForever,
-        OnEvery(Delay(0.1)),
-        function()
-          local pp = util.player:GetPlacement()
-          local pf = fan:GetPlacement()
-          local closeToFan = true
-          closeToFan = closeToFan and pp.vy < pf.vy + 4 and pp.vy > pf.vy - 12
-          closeToFan = closeToFan and mthAbsF(pp.vx - pf. vx) < 4
-          closeToFan = closeToFan and mthAbsF(pp.vz - pf. vz) < 4
-          if closeToFan == initActivated then
-            switch:Deactivate()
-          else
-            switch:Activate()
-          end
-        end
-      )
-    end)
-  else
-    switch = entity
+RunHandled(
+  WaitForever,
+  OnEvery(Delay(0.1)),
+  function()
+    if util.IsPlayerInArea(detector) then
+      fan:Deactivate()
+    else
+      fan:Activate()
+    end
   end
-end
+)
