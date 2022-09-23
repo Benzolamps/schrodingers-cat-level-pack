94c9c88cb88729ba1eac5aa3203e1baf0d4195cb
Benzolamps <gwy9826@163.com>
2022-09-23 23:22:22 +0800

    优化

--- Content/Talos/Levels/sc_level_pack/Scripts/Level043Reset.lua
+++ Content/Talos/Levels/sc_level_pack/Scripts/Level043Reset.lua
@@ -1,22 +1,11 @@
 local util = worldGlobals.CreateUtil()
 
-RunHandled(
-  function ()
-    Wait(Event(fan.Activated))
-  end,
-  OnEvery(Event(detector.Activated)),
-  function ()
-    detector:Recharge()
-    util.ResetMessage()
-  end
-)
-
 Wait(Event(mine.Jammed))
 RunHandled(
-  util.WaitTerminal,
+  WaitForever,
   OnEvery(Delay(0.1)),
   function ()
-    if util.player:GetPlacement().vy < 0.1 and not switch:IsActivated() then
+    if util.player:GetPlacement().vy < 0.2 and not switch:IsActivated() then
       util.ResetMessage()
     end
   end
